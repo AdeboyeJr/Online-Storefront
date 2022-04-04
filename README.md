74e2018ab994f0f7cfaf2a5ecc135d3fa80a8fa1
Adeboye Adegbenro <adegben1@cse.msu.edu>
2022-04-04 18:35:03 -0400

    Finished README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Online Storefront
+
+## Author: Adeboye Adegbenro Jr.
+
+#### Description
+
+A simple Online Storefront. Users can view catalogs of books, hats, clothes, games and more. Listings include a short description, price, colors and sizes.
+
+#### Implementation
+
+The web application begins with the `seestore.php` page. This is the catalog for the web app that makes MySQL queries to extract data on the different categories and store items. The first query is for showing all the categories available on the site.
+
+```php
+$get_cats_sql = "SELECT id, cat_title, cat_desc FROM store_categories ORDER BY cat_title";
+```
+
+Within each category are the list of items that include a link to their respective pages. The script embeds a hyperlink to itself and embeds the category id as a GET request parameter. Selecting a category loads the category id in the URL. A bullet list displays the items in available in the selected category. Upon clicking the item, the user is directed to the `showitem.php` page. This script queries the database for details on the selected item, which includes the name, description, price, and image.
+
+```php
+$get_item_sql = "SELECT c.id as cat_id, c.cat_title, si.item_title, si.item_price, si.item_desc, si.item_image FROM store_items AS si LEFT JOIN store_categories AS c
+ON c.id = si.cat_id WHERE si.id = '".$safe_item_id."'";
+```
+
+The `seestore_withJS.php` script is identical to `seestore.php` except that it includes premade CSS styling scripts and JavaScript code. This script creates a liquid slider to preview items in each catelog. The script also uses JQuery to select a category by its id and change its presentation.
+
+#### Dependencies
+
+You need a web browser that supports JavaScript.
---
+++ seestore_withJS.php
@@ -0,0 +1,97 @@
+<?php
+// connect to database
+$mysqli = mysqli_connect("localhost", "u667897109_Ade", "T#st1125", "u667897109_Data");
+
+$display_block = "<h1>My Categories</h1>
+<p>Scroll through the items in each category.</p>";
+
+// show categories first
+$get_cats_sql = "SELECT id, cat_title, cat_desc FROM store_categories ORDER BY cat_title";
+
+$get_cats_res = mysqli_query($mysqli, $get_cats_sql) or die(mysqli_error($mysqli));
+
+if (mysqli_num_rows($get_cats_res) < 1) {
+    $display_block = "<p><em>Sorry, no categories to browse.</em></p>";
+
+} else {
+    while ($cats = mysqli_fetch_array($get_cats_res)) {
+        $cat_id = $cats['id'];
+        $cat_title = strtoupper(stripslashes($cats['cat_title']));
+        $cat_desc = stripslashes($cats['cat_desc']);
+
+      
+        $display_block .= "<h2>".$cat_title."</h2>\n<p>".$cat_desc."</p>";
+
+        // get items
+        $get_items_sql = "SELECT id, item_title, item_price, item_desc, item_image FROM store_items WHERE cat_id = '".$cat_id."' ORDER BY item_title";
+
+        $get_items_res = mysqli_query($mysqli, $get_items_sql) or die(mysqli_error($mysqli));
+
+        if (mysqli_num_rows($get_items_res) < 1) {
+            $display_block = "<p><em>Sorry, no items in this category.</em></p>";
+        } else {
+            $display_block .= "<section class=\"liquid-slider\" id=\"main-slider-".$cat_id."\">";
+
+            while ($items = mysqli_fetch_array($get_items_res)) {
+                $item_id = $items['id'];
+                $item_title = stripslashes($items['item_title']);
+                $item_img = $items['item_image'];
+                $item_price = $items['item_price'];
+                $item_desc = $items['item_desc'];
+
+                $display_block .= <<<END_OF_TEXT
+                <div>
+                <h2 class="title">$item_title</h2>
+                <p>
+                <img src="images/$item_img" alt="$item_title" width="500" height="500" style=" float: left; margin-right: 0.5rem;">
+                $item_desc
+                </p>
+                <p>Price: \$$item_price</p>
+                <p><a href="seestore.php?cat_id=$cat_id"><button id="">Buy Now</button></a></p>
+                </div>
+                END_OF_TEXT;
+            }
+
+            $display_block .= <<<END_OF_TEXT
+
+            </section>
+            <script type="text/javascript">
+            $(function() {
+                $('#main-slider-$cat_id').liquidSlider({
+                    dynamicTabs: false,
+                    hoverArrows: false
+                });
+
+            });
+            </script>
+            END_OF_TEXT;
+        }
+        // free results
+        mysqli_free_result($get_items_res);
+    
+
+    }
+}
+// free results
+mysqli_free_result($get_cats_res);
+
+// close connection to MySQL
+mysqli_close($mysqli);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Categories</title>
+    <link rel="stylesheet" href="node_modules/liquidslider/css/liquid-slider.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.min.js"></script>
+    <script src="node_modules/liquidslider/js/jquery.liquid-slider.min.js"></script>
+</head>
+<body>
+    <?php echo $display_block; ?>
+</body>
+</html>
